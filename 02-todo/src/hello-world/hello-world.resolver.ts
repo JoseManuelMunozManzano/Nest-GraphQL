177354018e9daeabca41b559243d2bb81f13f85c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-05-14 21:12:24 +0200

    2-Generar un nuevo query

--- 02-todo/src/hello-world/hello-world.resolver.ts
+++ 02-todo/src/hello-world/hello-world.resolver.ts
@@ -1,6 +1,6 @@
 // Este Resolver lo creamos después del module con el mandato CLI
 // nest g r helloWorld --no-spec
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 // Proveen las instrucciones para transformar las instrucciones provenientes del cliente en data que GraphQL
 // puede utilizar. Los resolvers son similares a los controladores tradicionales de un REST endpoint con Nest,
@@ -12,16 +12,39 @@ import { Query, Resolver } from '@nestjs/graphql';
 //
 // Un ejemplo de query sería:
 //  {
-//    helloWorld
+//    hello
 //  }
 //
 // Nos devolverá: {"data": {"Hola Mundo"}}
+//
+// Otro ejemplo para que se vea que se puede llamar en la misma petición varias veces al mismo query:
+// query ObtieneNumeros {
+//   rnd1: randomNumber
+//   rnd2: randomNumber
+//   rnd3: randomNumber
+//   rnd4: randomNumber
+//   hello
+// }
 @Resolver()
 export class HelloWorldResolver {
   // Además de indicar a nivel TypeScript que esta función devuelve un string, también hay que indicarle a
   // GraphQL que es lo que la función va a regresar.
-  @Query(() => String)
+  //
+  // En el decorador @Query podemos indicar el name. Si no se indica, hemos visto en Apollo que coge el nombre
+  // de la función.
+  // También podemos indicar un descriptivo. Así se puede ir montando la documentación.
+  @Query(() => String, {
+    description: 'Hola Mundo es lo que retorna',
+    name: 'hello',
+  })
   helloWorld(): string {
     return 'Hola Mundo';
+  }
+
+  // Los tipos de datos que no existen en NodeJs se exportan de @nestjs/graphql
+  @Query(() => Float, { name: 'randomNumber' })
+  // El number ya es propio de TypeScript.
+  getRandomNumber(): number {
+    return Math.random() * 100;
   }
 }
