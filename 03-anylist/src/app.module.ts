661e93792f9a2ae85821ba4ead6ac1a0dfa6daba
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-05-25 18:35:11 +0200

    3-Conectar .env y base de datos a Nest

--- 03-anylist/src/app.module.ts
+++ 03-anylist/src/app.module.ts
@@ -1,8 +1,13 @@
 import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
 import { ItemsModule } from './items/items.module';
 
 // Indicar que se pueden mezclar controladores con GraphQL.
@@ -12,12 +17,34 @@ import { ItemsModule } from './items/items.module';
 @Module({
   // Recordar que todo lo que sea Module irá en imports.
   imports: [
+    // Para poder leer las variables de entorno hay que instalar:
+    // yarn add @nestjs/config
+    // Y añadir esta configuración.
+    ConfigModule.forRoot(),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+
+    // Para la integración de Nest con TypeOrm mirar: https://docs.nestjs.com/techniques/database
+    // Hay que instalar:
+    // yarn add @nestjs/typeorm typeorm pg
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DB_NAME,
+      // Con synchronize al guardar automáticamente se crean las tablas de bases de datos.
+      // IMPORTANTE: En producción synchronize: false
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
     ItemsModule,
   ],
   controllers: [],
