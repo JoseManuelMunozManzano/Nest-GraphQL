72f779abc94cf6b532f166167574de1aacbdcba0
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-05-26 06:56:07 +0200

    3-Creando Entidad Item

--- 03-anylist/src/items/items.module.ts
+++ 03-anylist/src/items/items.module.ts
@@ -2,10 +2,16 @@
 // nest g res items --no-spec
 // Y seleccionando GraphQL (code first)
 import { Module } from '@nestjs/common';
-import { ItemsService } from './items.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ItemsResolver } from './items.resolver';
+import { ItemsService } from './items.service';
+import { Item } from './entities/item.entity';
 
 @Module({
   providers: [ItemsResolver, ItemsService],
+  // Se indica forFeature porque solo puede haber un forRoot por aplicación, especificado en app.module,
+  // y luego en un arreglo las entidades que va a tener este módulo
+  imports: [TypeOrmModule.forFeature([Item])],
 })
 export class ItemsModule {}
