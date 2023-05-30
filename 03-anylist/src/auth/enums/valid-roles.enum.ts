bfb70bead8361acb426bd40a6b3f115a6e3d7f4a
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-05-30 18:27:57 +0200

    3-GraphQL Enumerations

--- 03-anylist/src/auth/enums/valid-roles.enum.ts
+++ 03-anylist/src/auth/enums/valid-roles.enum.ts
@@ -1,8 +1,19 @@
+import { registerEnumType } from '@nestjs/graphql';
 // Enumeración, propia de TS para indicar los roles disponibles.
-// Lo acaberemos transformando en un objeto de tipo Enumeración de GraphQL para que este lo reconozca en Apollo.
-// TODO: Implementar enum como GraphQL Enum Type
+//
+// Las enumeraciones en GraphQL funcionan exactamente igual a las enumeraciones en TypeScript
+// https://graphql.org/learn/schema/#enumeration-types
 export enum ValidRoles {
   admin = 'admin',
   user = 'user',
   superUser = 'superUser',
 }
+
+// Para hacer que el enumerador sea entendible por GraphQL hay que registrar la enumeración.
+// En name va el nombre para GraphQL (para el schema). Igual la descripción.
+// Con esto, en Apollo, al definir los roles, solo podemos poder los indicados en la enumeración.
+// Si indicamos otro valor, el query es incorrecto y no va a llegar ni a ejecutarse (Ver Query AllUsers en Apollo)
+registerEnumType(ValidRoles, {
+  name: 'ValidRoles',
+  description: 'Roles permitidos',
+});
--- 03-anylist/src/users/dto/args/roles.arg.ts
+++ 03-anylist/src/users/dto/args/roles.arg.ts
@@ -8,7 +8,12 @@ import { ValidRoles } from '../../../auth/enums/valid-roles.enum';
 //  - Como luce el campo roles, con @Field
 @ArgsType()
 export class ValidRolesArgs {
-  @Field(() => [String], { nullable: true })
+  // Tenemos que indicar a GraphQL que tipo de dato es ValidRoles.
+  // Cuando estamos trabajando con un tipo de decorador que es propio de GraphQL, como @Field en este caso,
+  // tenemos que trabajar con tipos propios o tipos personalizados de GraphQL.
+  // Por eso tenemos que hacer que ValidRoles, una enumeración TypeScript, sea también una enumeración
+  // GraphQL.
+  @Field(() => [ValidRoles], { nullable: true })
   @IsArray()
   roles: string[] = [];
 }
