13aa5dd9d01e2ddc934a6ac9818bce08bc5bb83c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-06-01 20:25:25 +0200

    3-Bloquear schema de GraphQL con módulo asíncrono

--- 03-anylist/src/app.module.ts
+++ 03-anylist/src/app.module.ts
@@ -1,10 +1,11 @@
 import { join } from 'path';
 
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
@@ -24,12 +25,49 @@ import { AuthModule } from './auth/auth.module';
     // Y añadir esta configuración.
     ConfigModule.forRoot(),
 
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    // Vamos a bloquear el schema si el usuario no está autenticado (no hay token)
+    // Indicar que esto sólo tiene sentido si el login y el signup están en RESTFul porque si no,
+    // si tenemos bloqueado el schema, ¿cómo nos logueamos?
+    //
+    // La idea es bloquear GraphQLModule y para eso hay que transformarlo en un módulo asíncrono con
+    // su factory, y que el módulo JWTModule, que está en AuthModule, esté activo para poder obtener
+    // el JWT Token y poder validarlo.
+    //
+    // Un módulo asíncrono tiene sentido cuando tenemos alguna configuración que dependa de algún endpoint.
+    //
+    // Para probar esto, en Apollo quitar el check en los Headers, en la key Authorization
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: false,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      imports: [AuthModule],
+      // Permitiendo la inyección
+      inject: [JwtService],
+      // Inyectando
+      useFactory: async (jwtService: JwtService) => ({
+        playground: false,
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        // Como ver la información que trae nuestro schema y trabajar con ella.
+        context({ req }) {
+          // NOTA: Esto es para saber como se hace, pero tenemos que comentarlo porque si no no podremos hacer el
+          // login una vez expire.
+          //
+          // Cogemos el token del header (si viene, de ahí la ?) y con nuestros módulos de autorización vemos si es válido.
+          // const token = req.headers.authorization?.replace('Bearer ', '');
+          // if (!token) throw Error('Token needed');
+          // const payload = jwtService.decode(token);
+          // if (!payload) throw Error('Token not valid');
+          //console.log({ payload });
+        },
+      }),
     }),
+
+    //? Esta es la configuración básica de GraphModule
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   playground: false,
+    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    //   plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    // }),
 
     // Para la integración de Nest con TypeOrm mirar: https://docs.nestjs.com/techniques/database
     // Hay que instalar:
